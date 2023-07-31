fa3c92dfe4ccc9365fc0f628595fe98e666e8d99
LauGds <137927575+LauGds@users.noreply.github.com>
2023-07-31 13:25:20 -0700

    main.py

--- main.py
+++ main.py
@@ -14,20 +14,6 @@ parameters = {
     "lon": LON,
 }
 
-# response = requests.get(url=OWN_Endpoint, params=parameters)
-# response.raise_for_status()
-# weather_data = response.json()
-# weather_slice = weather_data["hourly"][:12]
-#
-# will_rain = False
-#
-# for hour_data in weather_slice:
-#     condition_code = hour_data["weather"][0]["id"]
-#     if int(condition_code) < 700:
-#         will_rain = True
-#
-# if will_rain:
-#     print("Bring an umbrella.")
 
 
 response = requests.get(url=OWN_Endpoint, params=parameters)
